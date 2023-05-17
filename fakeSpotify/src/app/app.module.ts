b4194a1e64bc5aac2015851878df0aafd13b702d
Karolina <karo129@wp.pl>
2023-05-17 20:31:05 +0200

    created components

--- fakeSpotify/src/app/app.module.ts
+++ fakeSpotify/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './shared/components/side-bar/side-bar.component';
+import { MediaPlayerComponent } from './shared/components/media-player/media-player.component';
+import { HeaderUserComponent } from './shared/components/header-user/header-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    MediaPlayerComponent,
+    HeaderUserComponent
   ],
   imports: [
     BrowserModule,
