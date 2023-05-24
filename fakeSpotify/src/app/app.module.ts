d873512d827dd093d1605d2964f00e1d9c3020aa
Karolina <karo129@wp.pl>
2023-05-24 19:14:14 +0200

    added router-outlet

--- fakeSpotify/src/app/app.module.ts
+++ fakeSpotify/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SideBarComponent } from './shared/components/side-bar/side-bar.component';
-import { MediaPlayerComponent } from './shared/components/media-player/media-player.component';
-import { HeaderUserComponent } from './shared/components/header-user/header-user.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SideBarComponent,
-    MediaPlayerComponent,
-    HeaderUserComponent
+  
   ],
   imports: [
     BrowserModule,
