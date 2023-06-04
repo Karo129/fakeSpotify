42e3276d0c1c60727523c613bf5baf2558d1d52c
Karolina <karo129@wp.pl>
2023-06-04 11:54:19 +0200

    added observable

--- fakeSpotify/src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ fakeSpotify/src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,21 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import * as dataRaw from '../../../../data/tracks.json'
 import { TrackModel } from '@core/models/tracks.model';
+import { TracksService } from '@modules/tracks/services/tracks.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tracks-page',
   templateUrl: './tracks-page.component.html',
   styleUrls: ['./tracks-page.component.css']
 })
-export class TracksPageComponent implements OnInit {
-  mockTracksList: Array<TrackModel>= [
-  ]
+export class TracksPageComponent implements OnInit, OnDestroy {
+  tracksTrending: Array<TrackModel>= []
+  tracksRandom: Array<TrackModel>= []
 
-  constructor() { }
+  listObservers$: Array<Subscription> = []
+
+  constructor(private trackService: TracksService) { }
 
   ngOnInit(): void {
-    const data: any = (dataRaw as any).default;
-   this.mockTracksList = data;
+   const observer1$ = this.trackService.dataTracksTrending$
+   .subscribe(response => {
+    this.tracksTrending = response;
+    console.log('Canciones trending', response);
+   })
+
+   const observer2$ = this.trackService.dataTracksRandom$
+   .subscribe(response => {
+    this.tracksRandom = [...this.tracksRandom, ...response];
+    console.log('Canciones random entrando...', response);
+   })
+
+
+
+   this.listObservers$ = [observer1$, observer2$]
+  }
+
+  ngOnDestroy(): void {
+    this.listObservers$.forEach(u => u.unsubscribe());
   }
 
 }
